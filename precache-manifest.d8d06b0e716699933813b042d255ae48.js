2fbe8139c86f2b2a153ae5d6932b90af7010a8f9
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-06-04 22:18:11 -0300

    Updates

---
+++ precache-manifest.d8d06b0e716699933813b042d255ae48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c677d4f11ba6b8f41f73c6d3fd0fa16d",
+    "url": "/semear/index.html"
+  },
+  {
+    "revision": "b81ea2d58b255c177070",
+    "url": "/semear/static/css/main.6344bb93.chunk.css"
+  },
+  {
+    "revision": "380f9eab5cae09a9612f",
+    "url": "/semear/static/js/2.3906e226.chunk.js"
+  },
+  {
+    "revision": "b81ea2d58b255c177070",
+    "url": "/semear/static/js/main.e114c082.chunk.js"
+  },
+  {
+    "revision": "152f0484b7eeed91d14d",
+    "url": "/semear/static/js/runtime~main.44080123.js"
+  }
+]);
