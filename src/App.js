b044f4c87a66fe9affe64d57fb20d5becf07cc65
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-06-09 05:03:48 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import WithChat from './hocs/WithChat';
+import WithAdmin from './hocs/WithAdmin';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <WithChat />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </Router>
   );
 }
+
+const Home = () => <WithChat />;
+
+const Admin = () => <WithAdmin />;
 
 export default App;
