abf184a5be53999568f0c494a976e5e6db0f8d59
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-06-09 05:04:10 -0300

    Add title and update theme

--- src/hocs/WithChat.js
+++ src/hocs/WithChat.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import ChatBot from 'react-simple-chatbot';
-import {data} from '../data/data.json';
+import {data, title} from '../data/data.json';
 import {ThemeProvider} from 'styled-components';
+import {database} from '../utils/firebase';
 
 const theme = {
-	background: '#FFF',
-	botBubbleColor: '#2E2D33',
-	botFontColor: '#FFF',
-}
+  background: '#FFF',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#2E2D33',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#2E2D33',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
-const WithChat = () => (
-	<ThemeProvider theme={theme}>
-		<ChatBot
-			hideHeader
-			steps={data}
-			width="100%"
-		/>
-	</ThemeProvider>
-);
+const WithChat = () => {
+	const handleEnd = ({renderedSteps, steps, values }) => {
+		const currentDate = new Date().getTime();
+
+		database.ref(`/users/${currentDate}`).set({
+			name: values[0],
+			option: values[1],
+			email: values[2]
+		});
+	}
+
+	return (
+		<ThemeProvider theme={theme}>
+			<ChatBot
+				handleEnd={handleEnd}
+				headerTitle={title}
+				steps={data}
+				width="100%"
+			/>
+		</ThemeProvider>
+	);
+};
 
 export default WithChat;
