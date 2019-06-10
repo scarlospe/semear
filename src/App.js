02decd8c86cf0a9a2fda1302b06855f3a5f5dd0d
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-06-10 00:14:20 -0300

    Add SPA redirect for gh-pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route path="/admin" component={Admin} />
